b167c657021081db35214700ce1c92362c458dd0
Andrew Red <andrew@reddikh.com>
2018-07-19 12:39:00 +0300

    - fixed file formatting

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-"use strict";
-
 const express = require('express');
 const debug = require('debug')('app');
 const kue = require('kue');
 const ui = require('kue-ui');
 
-const port = 3000;
-
 const redisConfig = {
-    host: process.env.REDIS_ENV_REDIS_HOST || 'redis',
-    port: process.env.REDIS_ENV_REDIS_PORT || '6379'
+  host: process.env.REDIS_HOST || 'redis',
+  port: process.env.REDIS_PORT || '6379',
 };
 
-if (process.env.REDIS_ENV_REDIS_PASS) {
-    redisConfig.auth = process.env.REDIS_ENV_REDIS_PASS;
+if (process.env.REDIS_PASSWORD) {
+  redisConfig.auth = process.env.REDIS_PASSWORD;
 }
 
 debug.log = console.log.bind(console);
 
 const queue = kue.createQueue({
-    redis: redisConfig
+  redis: redisConfig,
 });
-queue.on('error', function(err) {
-    debug('Queue error', err);
+queue.on('error', function (err) {
+  debug('Queue error', err);
 });
 
 ui.setup({
-    apiURL: '/api',
-    baseURL: '/kue',
-    updateInterval: 5000
+  apiURL: '/api',
+  baseURL: '/kue',
+  updateInterval: 5000,
 });
 
 const app = express();
-
 app
-    .use('/api', kue.app)
-    .use('/kue', ui.app)
-    .listen(port);
-
+  .use('/kue', ui.app)
+  .use('/api', kue.app)
+  .listen(3000);
 debug('Application started');
